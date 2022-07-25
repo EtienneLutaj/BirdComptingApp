df9e339b092b21a434db694abfc6f56bae025589
EtienneLutaj <lutaj.etienne@gmail.com>
2022-07-25 17:31:56 +0200

    réorganisation du code

---
+++ cmd/Main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/EtienneLutaj/BirdComptingApp/server"
+
+	"github.com/gin-gonic/gin"
+)
+
+//make(chan int) ?
+
+func PostBird(context *gin.Context) {
+
+	var newBird server.Bird
+
+	if err := context.BindJSON(&newBird); err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	context.IndentedJSON(http.StatusCreated, newBird)
+
+	server.RegisterNewBird(newBird)
+
+}
+
+func GetBird(context *gin.Context) {
+	birdName := context.Param("birdname")
+	number := server.GetNumberOfOneBird(birdName)
+
+	context.IndentedJSON(http.StatusOK, number)
+}
+
+func GetBirds(context *gin.Context) {
+	BirdMap := server.GetNumberOfAllBirds()
+	context.IndentedJSON(http.StatusOK, BirdMap)
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/bird", PostBird) // ne prend les JSON que 1 par 1 et ne prend pas de liste avec plusieurs JSON en entré.
+	router.GET("/bird/:birdname", GetBird)
+	router.GET("/bird", GetBirds)
+	router.Run("localhost:8080")
+}
---
+++ server/Server.go
@@ -0,0 +1,61 @@
+package server
+
+import "fmt"
+
+type Bird struct {
+	ID       string `json:"id"`
+	BirdName string `json:"birdname"`
+	//NumberOfBirds int    `json:"numberofbirds"`
+}
+
+var birds []Bird
+
+func RegisterNewBird(newBird Bird) {
+
+	birds = append(birds, newBird)
+
+	fmt.Printf("L'oiseau %v a bien été enregistré !\n", newBird.BirdName)
+
+}
+
+func GetNumberOfOneBird(birdName string) int {
+
+	a := 0
+
+	for _, b := range birds {
+
+		if b.BirdName == birdName {
+			a++
+		}
+
+	}
+
+	if a == 0 {
+		fmt.Printf("L'oiseau %v n'existe pas ou n'a jamais été aperçu pour l'instant.\n", birdName)
+	} else {
+		fmt.Printf("L'oiseau %v a été aperçu %v fois !\n", birdName, a)
+	}
+
+	return a
+
+}
+
+func GetNumberOfAllBirds() map[string]int {
+
+	BirdMap := make(map[string]int)
+
+	for _, bird := range birds {
+
+		v, ok := BirdMap[bird.BirdName]
+
+		if ok {
+			BirdMap[bird.BirdName] = v + 1
+		} else {
+			BirdMap[bird.BirdName] = 1
+		}
+
+	}
+
+	return BirdMap
+
+}
